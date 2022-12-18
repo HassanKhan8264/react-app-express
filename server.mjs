283a9fa84ac2b988056c88b803069bbdf66173c4
HassanKhan8264 <hf610061@gmail.com>
2022-12-18 17:12:59 +0500

    new build

--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ app.get('/abc', (req, res) => {
 
 const __dirname = path.resolve()
 
-app.use('/', express.static(path.join(__dirname, '/web/build')))
+app.use('/', express.static(path.join(__dirname, '/react-app/build')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
